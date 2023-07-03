a8dad3b8e2255ac59d56f7451af327f07241d20e
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-07-03 23:03:18 +0900

    Interceptor.excludePathPatterns로 error페이지 인터셉터 안걸림

--- src/main/java/hello/exception/config/WebConfig.java
+++ src/main/java/hello/exception/config/WebConfig.java
@@ -1,9 +1,12 @@
 package hello.exception.config;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -12,7 +15,7 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
@@ -23,4 +26,13 @@ public class WebConfig implements WebMvcConfigurer {
         return filterFilterRegistrationBean;
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                //excludePathPatterns으로 error 인터셉터 안 걸리게
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");
+
+    }
 }
