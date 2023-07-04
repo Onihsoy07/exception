83275a9c01494d3b4397964a2ea61bfdb6342869
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-07-04 22:57:30 +0900

    @ResponseStatus로 HttpStatusCode 변경

---
+++ src/main/java/hello/exceptionexception/BadRequestException.java
@@ -0,0 +1,10 @@
+package hello.exceptionexception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "잘못된 요청 오류")
+public class BadRequestException extends RuntimeException {
+
+
+}
