3eca139c87c85c19dd35498b952d6f2e299a75c7
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-07-05 23:26:23 +0900

    @ControllerAdvice로 에러 처리 분리

--- src/main/java/hello/exception/api/ApiExceptionV3Controller.java
+++ src/main/java/hello/exception/api/ApiExceptionV3Controller.java
@@ -5,16 +5,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api2")
-public class ApiExceptionV2Controller {
+@RequestMapping("/api3")
+public class ApiExceptionV3Controller {
 
     @GetMapping("/members/{id}")
     public MemberDto getMember(@PathVariable String id) {
-
+                
         if (id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
         }
